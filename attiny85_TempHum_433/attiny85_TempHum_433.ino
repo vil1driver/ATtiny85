c54a75f50307877413ceb7f011ad4fc99c48e621
Olivier <vil1driver@gmail.com>
2016-03-20 17:41:46 +0100

    graph align

--- attiny85_TempHum_433/attiny85_TempHum_433.ino
+++ attiny85_TempHum_433/attiny85_TempHum_433.ino
@@ -28,14 +28,14 @@
 
 /************************************************************
 
-		emplacement des PIN de la puce ATtiny85
+	emplacement des PIN de la puce ATtiny85
 
                          +-/\-+
 Ain0       (D  5)  PB5  1|    |8   VCC
 Ain3       (D  3)  PB3  2|    |7   PB2  (D  2)  INT0  Ain1
 Ain2       (D  4)  PB4  3|    |6   PB1  (D  1)        pwm1
-				   GND  4|    |5   PB0  (D  0)        pwm0
-						 +----+	
+		   GND  4|    |5   PB0  (D  0)        pwm0
+			 +----+	
 
 						 
 ****************       Confuguration       *****************/
