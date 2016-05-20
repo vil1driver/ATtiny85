14fe9a32fc2900717cc7607b75888f8a41f42dc1
Olivier <vil1driver@gmail.com>
2016-05-20 15:05:23 +0200

    Update attiny85_TempHumPir_433.ino

--- attiny85_TempHumPir_433/attiny85_TempHumPir_433.ino
+++ attiny85_TempHumPir_433/attiny85_TempHumPir_433.ino
@@ -525,13 +525,8 @@ void setup()
 // system wakes up when wtchdog is timed out
 void system_sleep() {
   cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
-
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
-  sleep_enable();
-
-  sleep_mode();                        // System sleeps here
-
-  sleep_disable();                     // System continues execution here when watchdog timed out 
+  sleep_mode();                        // Go to sleep
   sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
 }
 
